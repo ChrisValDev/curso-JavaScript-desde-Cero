790e9690fab155182cd3ceea0ff768803588be09
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-12-21 19:28:04 -0600

    Challenge: (sin resolver)

---
+++ src/09-challenge.js
@@ -0,0 +1,14 @@
+/* 
+Juguemos a adivinar la palbra.
+
+El usuario tiene 3 intentos para adivinar la palabra oculta.
+
+Requerimientos:
+
+- El juego debe tener una palabra oculta.
+- El juego puede dar 1 pista de la palabra.
+- El usuario debe ingresar una suposicion.
+- El juego debe verificar si la suposicion del usuario es correcta.
+- El juego debe tener un numero limitado de intentos.
+- El juego debe terminar cuando el usuario adivine la palabra o se quede sin intentos.
+*/
